85a50c628df75cbfb9e00d695cba4d31de044f43
brennemanw17 <brennemanw17@students.ecu.edu>
2021-04-11 11:12:07 -0400

    Adding modularity + Some Data
    Made the tweet scraper into a function so it can be called by main.py also adding in some tweet data for '$fb'

---
+++ main.py
@@ -0,0 +1,4 @@
+from tweets import *
+
+tweetScrape("$fb", "2021-04-05", "2021-04-06")
+
--- tweets.py
+++ tweets.py
@@ -7,19 +7,17 @@ auth = tw.OAuthHandler(apiconfig.consumer_key, apiconfig.consumer_secret)
 auth.set_access_token(apiconfig.access_token, apiconfig.access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
+def tweetScrape(keyword, dfrom, dto):
+    keyname = keyword.replace('$', '')
+    keyname = keyname.replace(' ', '')
+    timestmp = time.strftime("%Y-%m-%d-%H-%M-%S")
+    filename = "data/" + str(keyname + timestmp) + ".csv"
 
-keyword = "$fb"
-keyname = keyword.replace('$', '')
-keyname = keyname.replace(' ', '')
-date = "2021-04-05"
-timestmp = time.strftime("%Y-%m-%d-%H-%M-%S")
-filename = "data/" + str(keyname + timestmp) + ".csv"
+    tweets = tw.Cursor(api.search, q=keyword, lang="en", since=dfrom, until=dto).items(300)
 
-tweets = tw.Cursor(api.search, q=keyword, lang="en", since=date, until=date).items(300)
+    csvFile = open(filename, 'a', encoding="utf-8")
+    csvWriter = csv.writer(csvFile)
+    for tweet in tweets:
+        csvWriter.writerow([tweet.created_at, tweet.text.replace('RT', '')])
 
-csvFile = open(filename, 'a', encoding="utf-8")
-csvWriter = csv.writer(csvFile)
-for tweet in tweets:
-    csvWriter.writerow([tweet.created_at, tweet.text.replace('RT', '')])
-
-print("Completed.")
+    print("Completed.")
