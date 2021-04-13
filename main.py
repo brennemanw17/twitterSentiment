99a923d15396b68f933dd1faa1de3ce6fe959d37
brennemanw17 <brennemanw17@students.ecu.edu>
2021-04-12 21:40:23 -0400

    Added capability
    driver now processes a list of files instead of one at time, and still outputs as a single file so it can combine multiple input tweet files

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ from tweetscraper.tweets import *
 
 from sentiment.driver import *
 
-run("fb2021-04-11-11-10-03.csv")
+fbFiles = ["fb2021-04-11-11-10-03.csv","fb2021-04-11-11-35-10.csv", "fb2021-04-11-11-40-58.csv",
+           "fb2021-04-11-11-41-24.csv", "fb2021-04-11-12-08-30.csv"]
+
+
+run(fbFiles, "preproccesed/fb2021processed.csv")
+
--- sentiment/driver.py
+++ sentiment/driver.py
@@ -4,37 +4,26 @@ import os
 from sentiment.tokenizer import *
 import csv
 
-def run(doc):
+def run(docs, fname):
     temp = { }
-    filename = "preproccesed/analyzed" + doc
-    doc = "data/" + doc
-    with open(doc, 'r', encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    for doc in docs:
+        filename = "preproccesed/analyzed" + doc
+        doc = "data/" + doc
+        with open(doc, 'r', encoding='utf-8') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
 
-        for row in csv_reader:
-            if row:
-                temp[row[0]] = porterstemmer(stopwordremover(tokenizer(row[1]),
-                                                             getstopwords("sentiment/stopwords.txt")))
+            for row in csv_reader:
+                if row:
+                    temp[row[0]] = porterstemmer(stopwordremover(tokenizer(row[1]),
+                                                                 getstopwords("sentiment/stopwords.txt")))
 
-    with open(filename, 'a', encoding="utf-8") as csvFile:
+    with open(fname, 'a', encoding="utf-8") as csvFile:
         csvWriter = csv.writer(csvFile)
         for key in temp:
             csvWriter.writerow([key, temp[key]])
     print("completed")
 
 
-
-# documap reads all files in a given directory then
-# uses the docuindex function to tokenize, stem and
-# remove stopwords from each file read. After docuindex
-# is used the returned dictionary is stored in another
-# dictionary which holds all documents in the directory
-def documap(directory):
-    documapped = {}
-    with os.scandir(directory) as files:
-        for file in files:
-            documapped[file.name] = docuindex(directory + "/" + file.name)
-    return documapped
 
 
 
