85e1d6e400c4cd820de7fb1c3fcacef79210bb43
lukebrush <brushl18@students.ecu.edu>
2021-04-14 00:16:59 -0400

    Cleaned up code

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-from tweetscraper.tweets import *
-import nltk
 from sentiment.driver import *
 
+"""
 fbFiles = ["fb2021-04-11-11-10-03.csv", "fb2021-04-11-11-35-10.csv", "fb2021-04-11-11-40-58.csv",
            "fb2021-04-11-11-41-24.csv", "fb2021-04-11-12-08-30.csv"]
 amznFiles = ["AMZN2021-04-11-11-42-47.csv", "AMZN2021-04-11-11-43-23.csv", "AMZN2021-04-11-11-43-39.csv",
              "AMZN2021-04-11-11-44-00.csv", "AMZN2021-04-11-12-10-23.csv"]
 tslaFiles = ["TSLA2021-04-11-12-07-07.csv", "TSLA2021-04-11-12-07-23.csv", "TSLA2021-04-11-12-07-36.csv",
              "TSLA2021-04-11-12-07-51.csv", "TSLA2021-04-11-12-08-06.csv"]
+"""
 
 # ------------- Example function calls
 
 # --- how to call tweet scraper
 # tweetScrape("$AMC", "2021-04-11", "0", 300)
 
+# print(termfreq("preproccesed/fb2021processed.json"))
+
+# amznSet = dataSet(amznFiles)
+# fbSet = dataSet(fbFiles)
+# tslaSet = dataSet(tslaFiles)
+
+# dataSets = [amznSet, fbSet, tslaSet]
+# print(positiveSet.vocab)
+
+
+# Test data set placeholder
 testData = dataSet(["947-negatives.csv"])
 
 
 # --- Analysis functions
 
+# Converts the training CSV's into a usable format for our program
 def vocabtolist():
     terms = []
     positiveSet = dataSet(["positive-Sentiment.csv"])
@@ -35,6 +47,7 @@ def vocabtolist():
     return terms
 
 
+# Converts json files to a list object
 def jsontolist(doc):
     terms = []
     with open(doc, 'r', encoding='utf-8') as json_file:
@@ -46,8 +59,8 @@ def jsontolist(doc):
 
 
 trainingSet = vocabtolist()
-# "positiveprocessed.json"
 
+# Various Test Data Sets
 
 # testSet = jsontolist("preproccesed/fb2021processed.json")
 # testSet = jsontolist("preproccesed/AMZN2021processed.json")
@@ -55,31 +68,28 @@ trainingSet = vocabtolist()
 testSet = jsontolist("preproccesed/947processed.json")
 
 
-# print(termfreq("preproccesed/fb2021processed.json"))
-
-# amznSet = dataSet(amznFiles)
-# fbSet = dataSet(fbFiles)
-# tslaSet = dataSet(tslaFiles)
-
-# dataSets = [amznSet, fbSet, tslaSet]
-# print(positiveSet.vocab)
-
 # ------------------------------------------------------------------------------------------------------------------------------
 
-def buildVocabList(trainingSet):
+
+# Builds a word bank that contains every work in the training set given to it
+def buildVocabList(trainingSetData):
     all_words = []
 
-    for words in trainingSet:
-        all_words.extend(words)
+    for words in trainingSetData:
+        all_words.append(words[0])
 
     wordlist = nltk.FreqDist(all_words)
-    word_features = wordlist.keys()
+    word_features1 = wordlist.keys()
 
-    return word_features
+    # print(word_features1)
+
+    return word_features1
 
 
 # ------------------------------------------------------------------------------------------------------------------------------
 
+# Compares each word in the word bank (word_features) and gives it a label: either true or false, where true means
+# word is in the tweet and false means it is absent
 def getfeatures(tweet):
     tweet_words = set(tweet)
     features = {}
@@ -87,29 +97,36 @@ def getfeatures(tweet):
     for word in word_features:
         features['contains(%s)' % word] = (word in tweet_words)
 
+    # print(features)
+
     return features
 
 
 # ----------------------------------------------------------------------------------------------------------------------------
 
-
-print("Building vocabulary.")
+# Builds the vocabulary list
+print("Building vocabulary bank..")
 word_features = buildVocabList(trainingSet)
 # print(word_features)
 
-print("Classifying features.")
+
+# Classifies features within the training set
+print("Classifying features..")
 trainingFeatures = nltk.classify.apply_features(getfeatures, trainingSet)
 
-print("Running Data on Naive Bayes Classifier:")
+# Trains the model based on the features
+print("Training Data..")
 NBayesClassifier = nltk.NaiveBayesClassifier.train(trainingFeatures)
 
-print("Results")
+print("Results:")
 NBResultsLabels = [NBayesClassifier.classify(getfeatures(tweet[0])) for tweet in testSet]
 
 print(NBResultsLabels)
 print("")
 
+# Prints the positive/negative sentiment depending on however many postive/negative tweets there are out of the
+# total number of tweets
 if NBResultsLabels.count('positive') > NBResultsLabels.count('negative'):
-    print("NB Result Positive Sentiment " + str(100 * NBResultsLabels.count('positive') / len(NBResultsLabels)) + "%")
+    print("Positive Sentiment" + str(100 * NBResultsLabels.count('positive') / len(NBResultsLabels)) + "%")
 else:
-    print("NB Result Negative Sentiment " + str(100 * NBResultsLabels.count('negative') / len(NBResultsLabels)) + "%")
+    print("Negative Sentiment " + str(100 * NBResultsLabels.count('negative') / len(NBResultsLabels)) + "%")
